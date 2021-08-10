bc1841072adf234c31fa57f2a5bb39040a28efc4
Mehmet5353 <44751934+Mehmet5353@users.noreply.github.com>
2021-08-10 09:05:13 +0300

    Update Mean, Median and Mode.py

--- Mean, Median and Mode.py
+++ Mean, Median and Mode.py
@@ -3,8 +3,7 @@ from collections import Counter
 N=int(input())
 L=list(map(int, input().rstrip().split()))
 L.sort()
-A=list(set(L))
-C=[]
+
 mean=sum(L)/len(L)
 if N%2==0:
     median=(L[int(N/2)]+L[int(N/2)-1])/2
